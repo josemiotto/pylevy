9523291cda89311df5e332fcc962220b8db5f540
Aquiles Carattino <aquiles@aquicarattino.com>
2018-08-20 11:41:04 +0200

    Changed the docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 #
 import os
 import sys
-from unittest.mock import MagicMock
 sys.path.insert(0, os.path.abspath('../../../'))
 
 # -- Project information -----------------------------------------------------
@@ -44,15 +43,6 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-MOCK_MODULES = ['numpy', 'scipy']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
